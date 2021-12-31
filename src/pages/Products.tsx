75888d1b9d2dca3ac1625ccd014b541145a98f39
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-12-31 23:12:28 +0900

    feat: show checkout link button and generate link

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Product} from '../models/products';
 import {Filters} from '../models/filters';
 import {findAllByDisplayValue} from '@testing-library/react';
+import axios from 'axios';
 
 const Products = (props: {
   products: Product[],
@@ -10,6 +11,11 @@ const Products = (props: {
   lastPage: number,
 }) => {
   const [selected, setSelected] = useState<number[]>([])
+  const [notify, setNotify] = useState({
+    show: false,
+    error: false,
+    message: ''
+  })
 
   const search = (s: string) => {
     props.setFilters({
@@ -42,6 +48,37 @@ const Products = (props: {
     setSelected([...selected, id])
   }
 
+  const generate = async () => {
+    try {
+      const {data} = await axios.post('links', {
+        products: selected
+      })
+
+      setNotify({
+        show: true,
+        error: false,
+        message: `Link generated: http://localhost:5000/${data.code}`
+      })
+    } catch (e) {
+      setNotify({
+        show: true,
+        error: true,
+        message: 'You should be logged in to generate a link'
+      })
+    } finally {
+      // remove message so execute when ever
+      // after 3 sec message removed
+      setTimeout(() => {
+        setNotify({
+          show: false,
+          error: false,
+          message: ''
+        })
+      }, 3000)
+    }
+
+  }
+
   let button;
   if (props.filters.page != props.lastPage) {
     button = (
@@ -51,12 +88,34 @@ const Products = (props: {
     )
   }
 
+  let generateButton, info;
+
+  if (selected.length > 0) {
+    generateButton = (
+      <div className="input-group-append">
+        <button className="btn btn-info" onClick={generate}>Generate Link</button>
+      </div>
+    )
+  }
+
+  if (notify.show) {
+    info = (
+      <div className="col-md-12 mb-4">
+        <div className={notify.error ? "alert alert-danger" : "alert alert-info"} role="alert">
+          {notify.message}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
+      {info}
       <div className="col-md-12 mb-4 input-group">
         <input type="text" className="form-control" placeholder="Search"
           onChange={e => search(e.target.value)}
         />
+        {generateButton}
         <div className="input-group-append">
           <select className="form-select" onChange={e => sort(e.target.value)}>
             <option>Select</option>
